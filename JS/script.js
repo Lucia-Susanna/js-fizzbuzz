448a848895200c5d1fc7bf6d3f4ed5d20639d6ca
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-11-14 17:02:25 +0100

    feat: else

--- JS/script.js
+++ JS/script.js
@@ -5,20 +5,22 @@ const multiploDi3e5 = multiploDi3 + multiploDi5;
 
 //CICLO NUMERI DA 1 A 100
 for (let i = 1; i <=100; i++) {
-  
+   
   // CONDIZIONI
   if  (!(i % 3) && !(i % 5)){
-    console.log(multiploDi3e5);  
-    
+    console.log(multiploDi3e5);      
     
   } else if (!(i % 5)){
     console.log(multiploDi5);
     
   } else if (!(i % 3)){ 
     console.log(multiploDi3);  
-  }
 
-  console.log(i);
+  } else {
+    console.log(i);
+    
+  }
 }
+ 
 
 
